ca6646ef6bcdae254c09efb9215bf942115e8657
punityadav2512 <b520040@iiit-bh.ac.in>
2022-04-16 12:54:20 +0530

    comment

--- routes/post.js
+++ routes/post.js
@@ -6,4 +6,5 @@ const router = express.Router();
 router.route('/post/upload').post(isAuthenticated, createPost);
 router.route('/post/:id').get(isAuthenticated, likeAndUnlikePost).put(isAuthenticated, updateCaption).delete(isAuthenticated, deletePost);
 router.route('/posts').get(isAuthenticated, getPostOfFollowing);
+router.route('/post/comment/:id').put(isAuthenticated, commentOnPost);
 module.exports = router;
